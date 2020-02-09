6bfd961c2d3d354f7486ad53e2736e2d6cf2a7d4
giroberto <umbertobarros@gmail.com>
2020-02-08 23:53:58 -0300

    Repo data listing

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Api from '../../services/api';
 
-export default function Repository({ match }) {
-  return <div>Repo {decodeURIComponent(match.params.repository)}</div>;
+import Container from '../../components/Container';
+
+import { Loading } from './styles';
+
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      Api.get(`/repos/${repoName}`),
+      Api.get(`/repos/${repoName}/issues`, {
+        params: {
+          status: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+    return <Container>Repo {}</Container>;
+  }
 }
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
