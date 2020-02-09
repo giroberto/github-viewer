b043d6b3626340ab90cb9b5d5627b4c39954930d
giroberto <umbertobarros@gmail.com>
2020-02-09 01:39:18 -0300

    Update on repo view

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Api from '../../services/api';
 
 import Container from '../../components/Container';
 
-import { Loading } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 export default class Repository extends Component {
   state = {
@@ -38,9 +39,36 @@ export default class Repository extends Component {
     const { repository, issues, loading } = this.state;
 
     if (loading) {
-      return <Loading>Carregando</Loading>;
+      return <Loading>Loading</Loading>;
     }
-    return <Container>Repo {}</Container>;
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Return to repo list</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.owner.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.name} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.user.name}
+                  {issue.labels.map(label => (
+                    <span key={label.id}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
   }
 }
 Repository.propTypes = {
